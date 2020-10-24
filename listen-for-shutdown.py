03e4c8cbdb978e967812e18b93d757f05bc19261
zanhecht <ahecht@earthlink.net>
2020-10-24 11:25:15 -0400

    Update listen-for-shutdown.py
    Debounce

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -3,10 +3,15 @@
 
 import RPi.GPIO as GPIO
 import subprocess
-
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+while True:
+	GPIO.wait_for_edge(3, GPIO.FALLING)
+	start = time.time()
+	GPIO.wait_for_edge(3, GPIO.RISING)
+	elapsed = (time.time() - start)
+	if elapsed > 0.1:
+		subprocess.run(['shutdown', '-h', 'now'], shell=False)
